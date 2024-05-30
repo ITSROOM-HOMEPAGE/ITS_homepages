c34cab930a023db89d6c0c821322a5919afdd9ea
MUYEONKIM <rlaandus2@gmail.com>
2024-05-30 15:55:36 +0900

    convert Vue to React mainpage and add antd

---
+++ src/components/kakaomap/kakaomap.js
@@ -0,0 +1,60 @@
+import { useEffect } from "react";
+
+const { kakao } = window;
+
+export default function Kakaomap() {
+  useEffect(() => {
+    const mapScript = document.createElement("script");
+
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=1cb22d0e18abc571542487e53c65c3b2&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById("map");
+        const options = {
+          center: new window.kakao.maps.LatLng(
+            35.54214322212964,
+            129.31287511752132
+          ),
+          level: 2,
+        };
+        new window.kakao.maps.Map(container, options);
+        new window.kakao.maps.Marker({
+          position: options.center,
+        }).setMap(new window.kakao.maps.Map(container, options));
+        // const marker = new window.kakao.maps.Marker({
+        //   position: options.center,
+        // });
+        // const infowindow = new kakao.maps.InfoWindow({
+        //   position: options.center,
+        //   content:
+        //     '<div style="padding:5px; ">Sandburg Office<br/>ITS Space</div>',
+        // });
+        // marker.setMap(map);
+        // infowindow.open(map, marker);
+        // const marker = new window.kakao.maps.Marker({
+        //   position: options.center,
+        // });
+
+        const infowindow = new kakao.maps.InfoWindow({
+          position: options.center,
+          content:
+            '<div style="padding:5px; ">Sandburg Office<br/>ITS Space</div>',
+        });
+        // marker.setMap(map);
+        // infowindow.open(map, marker);
+      });
+    };
+    mapScript.addEventListener("load", onLoadKakaoMap);
+  }, []);
+
+  return (
+    <div
+      id="map"
+      className="text-black rounded-[24px] border-2 w-[1500px] h-[500px]"
+    ></div>
+  );
+}
---
+++ src/layouts/pageHeader.js
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+export default function PageHeader() {
+  // ```
+  // {
+  //     'border-[#4E43ED] border-b-2 text-[#4E43ED]':
+  //         routePath === '/about',
+  // },
+  // ```;
+  return (
+    <>
+      <div className="w-full flex items-center justify-center">
+        <div className="w-[1300px] flex items-center h-20 text-[20px] font-PretendSemiBold justify-between">
+          <div>
+            <img src="./logo192.png" className="w-[80px]" />
+          </div>
+          <div className="ml-auto flex h-full">
+            <Link
+              className="w-[192px] h-full flex items-center justify-center hover:text-[#4E43ED]"
+              to="/about"
+            >
+              About
+            </Link>
+            <Link
+              className="w-[192px] h-full flex items-center justify-center hover:text-[#4E43ED]"
+              to="/solution"
+            >
+              Solution
+            </Link>
+            <Link
+              className="w-[192px] h-full flex items-center justify-center hover:text-[#4E43ED]"
+              to="/team"
+            >
+              Team
+            </Link>
+            <Link
+              className="w-[192px] h-full flex items-center justify-center hover:text-[#4E43ED]"
+              to="/recruit"
+            >
+              Recruit
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
