474e635944767128741e2c452c857884aff948b6
soyoung <kerry0325@naver.com>
2024-06-14 16:04:54 +0900

    test soyoung
    test

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,6 +1,7 @@
 import Mainpage0 from "../../components/mainPage/mainPage0";
 import Mainpage1 from "../../components/mainPage/mainPage1";
 import Mainpage2 from "../../components/mainPage/mainPage2";
+import Mainpage3 from "../../components/mainPage/mainPage3";
 
 export default function MainPage() {
   console.log("qwe");
@@ -9,6 +10,7 @@ export default function MainPage() {
   return (
     <div>
       <Mainpage0 />
+      <Mainpage3 />
       <Mainpage1 />
       <Mainpage2 />
     </div>
